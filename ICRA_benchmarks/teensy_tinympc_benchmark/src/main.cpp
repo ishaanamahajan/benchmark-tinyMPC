73f286cef5285c42a106cbc4dc372deb832d4b53
Ishaan Mahajan <ishaanamahajan@gmail.com>
2025-03-16 00:14:58 -0400

    data

--- ICRA_benchmarks/teensy_tinympc_benchmark/src/main.cpp
+++ ICRA_benchmarks/teensy_tinympc_benchmark/src/main.cpp
@@ -2,7 +2,7 @@
 #undef F
 #include "admm.hpp"
 //#include "problem_data/rand_prob_tinympc_params.hpp"
-#include "problem_data/problem_96.hpp"
+#include "problem_data/problem_100.hpp"
 #include "types.hpp"
 #include "rho_benchmark.hpp"
 
